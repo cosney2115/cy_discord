5a5c90cffb3a5b22060be0e4d9356a7b12e7bc1f
Cosney <mateuszolsz29@gmail.com>
2025-11-27 14:21:44 +0100

    feat: Add Discord API interaction, channel, and message structures with an HTTP request handler.

--- http/main.lua
+++ http/main.lua
@@ -1,5 +1,6 @@
 ---@class RequestHandler
 ---@field token string
+---@field request fun(self: RequestHandler, method: string, endpoint: string, data?: table): promise
 RequestHandler = {}
 
 function RequestHandler:new(token)
@@ -15,21 +16,22 @@ function RequestHandler:new(token)
         }
 
         PerformHttpRequest('https://discord.com/api/v10' .. endpoint, function(statusCode, responseBody, headers)
-            if statusCode >= 200 and statusCode < 300 then
-                local success, body = pcall(json.decode, responseBody)
-
-                if not success then
-                    p:resolve(nil)
-                    return
-                end
-
-                p:resolve(body)
-            else
+            if statusCode < 200 or statusCode >= 300 then
                 p:reject({
                     statusCode = statusCode,
                     body = responseBody
                 })
+                return
             end
+
+            local success, body = pcall(json.decode, responseBody)
+
+            if not success then
+                p:reject(body)
+                return
+            end
+
+            p:resolve(body)
         end, method, data and json.encode(data) or "", headers)
 
         return p
